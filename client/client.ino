eeeb929edac7853344274f5ef4ad710418e2b126
Daniel Soto <danielrsoto@gmail.com>
2012-03-25 10:13:11 -0400

    this is successfully posting linear ramp to server

--- client/client.ino
+++ client/client.ino
@@ -8,19 +8,27 @@ char packet[64];
 
 void setup() {
   Serial.begin(9600);
+  
+  WiFly.begin();
+  
+  //SpiSerial.begin();
+  
+  //Wifly.setUart(&Spi
+  
+  WiFly.join("wifly", "", 0);
 }
 
 int value=0;
 
 void loop() {
-  delay(1000);
+  delay(10000);
 
   value++;
   
   // create data payload
   sprintf(packet, "time_stamp=2000-01-01T12:00&tag=ardfly&value=%d",value);
 
-  // post to server
+  // print post text to serial
   Serial.print("POST / HTTP/1.1\r\n");
   Serial.print("Host: 108.166.92.185:8000\r\n");
   Serial.print("Content-Length: ");
@@ -30,6 +38,20 @@ void loop() {
   
   Serial.println();
   Serial.println();
+  
+  // post to server
+  if (client.connect()) {
+    client.print("POST / HTTP/1.1\r\n");
+    client.print("Host: 108.166.92.185:8000\r\n");
+    client.print("Content-Length: ");
+    client.print(strlen(packet));
+    client.print("\r\n\r\n");
+    client.print(packet);
+  
+    client.println();
+    client.println();
+  }
+  
 }
 
 
