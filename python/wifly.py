a1b4747c2d3e61b713e72bbe04adb2d16994f987
Daniel Soto <drdrsoto@gmail.com>
2012-06-03 13:10:21 -0400

    this code is clumsily downloading my webpage repeatedly

--- python/wifly.py
+++ python/wifly.py
@@ -4,9 +4,18 @@ import time
 
 delay = 5
 
+def configure():
+    print 'configuration'
+    # set authorization mode
+    # set authorization key
+    # set comm remote 0
+    # join network
+
 def send_command(command):
     print 'sent command:', command
-    s.write(command)
+    for c in command:
+        s.write(c)
+        time.sleep(0.1)
 
 def get_reply(s):
     reply = s.readlines()
@@ -18,9 +27,13 @@ s = serial.Serial('/dev/tty.usbserial-A700fpBT',
                   baudrate=9600)
 verbose = True
 while 1:
+    print
     print 'top of loop'
+    print '--------------------------------'
 
     # enter command mode
+    print
+    print 'enter command mode'
     send_command('$$$')
     time.sleep(delay)
 
@@ -29,6 +42,29 @@ while 1:
         print 'entered command mode'
     else:
         print 'did not enter command mode'
+
+
+    # connect to server
+    print
+    print 'connect to server'
+    print 'flushing buffer'
+    time.sleep(delay)
+    reply = get_reply(s)
+    send_command('open 50.56.226.226 80\r\n')
+    reply = get_reply(s)
+    if '*OPEN*' in reply:
+        print 'connected to server'
+        time.sleep(delay)
+        print
+        print 'sending GET request'
+        send_command('GET / HTTP/1.0\r\n\r\n')
+        time.sleep(delay)
+        reply = get_reply(s)
+    else:
+        print 'no connection to server'
+
+    # check for successful close
+
 
     # exit command mode
     send_command('exit\r\n')
@@ -41,9 +77,9 @@ while 1:
         print 'unsuccessful exit'
 
     '''
-    s.write('open 50.56.226.226 80\r\n')
     if '*OPEN*' in s.readlines():
         print 'connected to server'
+
 
     print s.readlines()
     s.write('GET / HTTP/1.0\r\n\r\n')
